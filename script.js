1640ba6f844136047ba6e7d09b3e98c953f2d432
Bruno <bruno.3av@gmail.com>
2025-10-03 19:21:49 -0300

    update url

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let socket; // Declare o socket como uma variável global
 
 function initializeWebSocket() {
-    socket = new WebSocket("ws://choice-collie-suitably.ngrok-free.app");
+    socket = new WebSocket("wss://choice-collie-suitably.ngrok-free.app");
 
     socket.addEventListener("open", function () {
         console.log("Conectado ao servidor WebSocket");
